2b6ae0b6ccf22903da51fbe251e882d2b1cc26c2
Robin Dupret <robin.dupret@gmail.com>
2017-06-12 22:05:20 +0200

    Distinguish the cover from the rest of the document with h-rules
    People can use an h-rule inside their document and the first one
    will be picked as a separator to distinguish the cover from the
    rest of the document.
    
    At the moment, this will not produce any change for the final PDF.

--- lib/mdown2pdf.rb
+++ lib/mdown2pdf.rb
@@ -5,38 +5,78 @@ require "mdown2pdf/version"
 module Mdown2PDF
   extend self
 
-  # Creates a temporary file to store the Markdown document once it's
-  # parsed so Wkhtmltopdf can render it.
-  def temporary_html_file_for(file)
-    path = "/tmp/output-#{(rand * 100).round}.html"
+  # Creates two temporary files:
+  #
+  #  * One to represent the eventual cover of the PDF file.
+  #  * One with the document translated to HTML.
+  #
+  # These two temporary files will be used by Wkhtmltopdf to
+  # generate the final PDF file.
+  #
+  # This method requires a block that will yield a path for each
+  # file. For example:
+  #
+  #   temporary_files_for("a_file.md") do |output, cover|
+  #     # Here `output` and `cover` are paths.
+  #   end
+  #
+  # To distinguish the cover from the rest of the document, the
+  # user must put an h-rule inside their document and the first
+  # one will be used as a separator.
+  def temporary_html_files_for(file)
+    seed        = (rand * 100).round
+    output_file = "/tmp/output-#{seed}.html"
+    cover_file  = "/tmp/cover-#{seed}.html"
 
-    File.open(path, "w") do |f|
-      f.write('<!DOCTYPE html>')
-      f.write('<html>')
-      f.write('<head>')
-      f.write('<meta charset="utf-8">')
-      f.write('</head>')
-      f.write('<body><div class="markdown-body" style="width: 960px; margin: auto">')
-      f.write(markdown_for(file))
-      f.write('</div></body>')
+    document = markdown_for(file)
+    first_hr = document.index('<hr>')
+
+    if first_hr
+      cover    = document[0..first_hr]
+      document = document[first_hr+4..-1]
     end
 
-    yield(path)
+    File.open(output_file, "w") do |f|
+      f.write(File.read(asset('output.html')) % { content: document })
+    end
+
+    if cover
+      File.open(cover_file, "w") do |f|
+        f.write(File.read(asset('output.html')) % { content: cover })
+      end
+
+      yield(output_file, cover_file)
+    else
+      yield(output_file, nil)
+    end
   ensure
-    File.delete(path)
+    File.delete(output_file)
+    File.delete(cover_file) if File.exist?(cover_file)
   end
 
   # Actually outputs a given HTML file as a PDF for a given name
   # through the `wkhtmltopdf` command line tool.
-  def output(file: , name: )
-    arguments = [
-      "wkhtmltopdf",
+  #
+  #  - The given `:file` key represents the HTML file to translate
+  #    to PDF.
+  #  - `:name` will be used to infer the PDF file's name.
+  #  - `:cover` can be used if a document should be used as a cover.
+  def output(file: , name: , cover: nil)
+    stylesheet = ["--user-style-sheet", asset('style.css')]
+    arguments  = ["wkhtmltopdf"]
+
+    if cover
+      arguments.push("cover", cover)
+      arguments.push(*stylesheet)
+    end
+
+    arguments.push(
       file,
       "--include-in-outline",
       "--enable-internal-links",
-      "--user-style-sheet", asset('style.css'),
+      *stylesheet,
       name.sub(/.(md|mdwon)/, '.pdf')
-    ]
+    )
 
     `#{arguments.join(" ")}`
   end
